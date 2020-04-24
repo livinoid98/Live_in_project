76ac4e8a9036e1ceae991b68dbb84843d96f72cb
HamJinkyoung <wlsrud3803@gmail.com>
2020-04-25 01:57:19 +0900

    model 구현(수정필요)

--- health_diary/models.py
+++ health_diary/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    category = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    Post_id = models.ForeignKey('Post', on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.content[:50]
--- mypage/admin.py
+++ mypage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Diary, Calendar
 
 # Register your models here.
+admin.site.register(Diary)
+admin.site.register(Calendar)
