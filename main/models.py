a41a3f62e10d9f2f19b90c18f24aa38acff5ed95
HamJinkyoung <wlsrud3803@gmail.com>
2020-04-28 17:26:50 +0900

    Hospital, Review 모델 수정

--- main/models.py
+++ main/models.py
@@ -1,24 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Hospital(models.Model):
-    name = models.CharField(max_length=20)
-    zone_category = models.CharField(max_length=20)
-    info = models.TextField()
-    site_url = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    tel = models.CharField(max_length=15)
-    img = models.ImageField()
-
-    def __str__(self):
-        return self.name
-
-class Review(models.Model):
-    Hospital_id = models.ForeignKey('Hospital', on_delete=models.CASCADE)
-    score = models.IntegerField()
-    content = models.CharField(max_length=200)
-    # User_id = models.ForeignKey('User')
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.content[:10]
