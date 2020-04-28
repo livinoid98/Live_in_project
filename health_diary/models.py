4db04dc7c460431b2c8784b3143a8af0b6489459
HamJinkyoung <wlsrud3803@gmail.com>
2020-04-28 17:39:55 +0900

    투병일기관련(Post, Comment) 모델 수정

--- health_diary/models.py
+++ health_diary/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
+    # User_id = models.ForeignKey('User')
     pub_date = models.DateTimeField('date published')
-    category = models.IntegerField()
 
     def __str__(self):
         return self.title
@@ -13,7 +13,8 @@ class Post(models.Model):
 class Comment(models.Model):
     Post_id = models.ForeignKey('Post', on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    # User_id = models.ForeignKey('User')
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content[:50]
