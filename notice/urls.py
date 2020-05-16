d7731ba29fd83ba3964ceef57ad3a402cf0a8c6e
HamJinkyoung <wlsrud3803@gmail.com>
2020-05-16 22:11:38 +0900

    notice 기본 CRUD구현(전문가,비전문가 구분 X)

--- notice/urls.py
+++ notice/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='notice'),
+    path('new/', views.boardform, name="new"),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/remove/', views.remove, name='remove'),
+    path('<int:board_id>', views.detail, name="detail"),
 ]
