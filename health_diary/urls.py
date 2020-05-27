ad7eb8deaf2a882cec1beedc33ea63d7b2f1b09d
HamJinkyoung <wlsrud3803@gmail.com>
2020-05-27 12:27:29 +0900

    remove_comment_hd로 urlname 변경
    notice에 있는 remove_comment랑 urlname 똑같아서

--- health_diary/urls.py
+++ health_diary/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('<int:pk>/remove/', views.remove, name='remove_hd'),
     path('<int:post_id>', views.detail, name="detail_hd"),
     # path('<int:comment_id>/edit_comment/', views.edit_comment, name='edit_comment'),
-    path('<int:comment_id>/remove_comment/', views.remove_comment, name='remove_comment'),
+    path('<int:comment_id>/remove_comment/', views.remove_comment, name='remove_comment_hd'),
 ]
