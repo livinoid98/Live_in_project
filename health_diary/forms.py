0b5e942966cb378ff172faee86b797261a94e857
HamJinkyoung <wlsrud3803@gmail.com>
2020-06-22 15:58:06 +0900

    health_diary, notice front update

--- health_diary/forms.py
+++ health_diary/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import TextInput, Textarea
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -9,4 +9,15 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': TextInput(attrs={'class':'floatLabel', 'placeholder':'Title'}),
             'content' : Textarea(attrs={'class':'floatLabel', 'placeholder':'Content'}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content' : forms.TextInput(attrs={'class':'comment-content', 'placeholder':'댓글을 입력하세요.'}),
+        }
+        labels = {
+            'content' : '',
         }
